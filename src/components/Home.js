ed6126055641d82f0734c5702c746270ee5264a9
claudiasolbes <cssolbes@hotmail.com>
2019-11-24 02:08:55 -0500

    added scroll button to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,34 +8,43 @@ class Home extends Component {
   render(){
     return (
       <div>
-        <img src="./images/backgrounds/unicorn.png" className="Home" alt="">
-        </img>
-        <HomeGreeting 
-          user={this.props.user}
-        />
-        <br></br>
-        <Button
-          className="ordersButton"
-          as={Link}
-          to={`/${this.props.user.username}/orders`}
-        > Orders
-        </Button>
-        <br></br>
-        <br></br>
-        <Button
-          className="settingsButton"
-          as={Link}
-          to={`/${this.props.user.username}/settings`}
-        > Settings
-        </Button>
-        <br></br>
-        <br></br>
-        <CakeList
-          cakes={this.props.cakes}
-          showCake={this.props.showCake}
-          findCake={this.props.findCake}
-        />
-        <br></br>
+        <div>
+          <img src="./images/backgrounds/unicorn.png" className="Home" alt="">
+          </img>
+          <HomeGreeting 
+            user={this.props.user}
+          />
+          <br></br>
+          <Button
+            className="ordersButton"
+            as={Link}
+            to={`/${this.props.user.username}/orders`}
+          > Orders
+          </Button>
+          <br></br>
+          <br></br>
+          <Button
+            className="settingsButton"
+            as={Link}
+            to={`/${this.props.user.username}/settings`}
+          > Settings
+          </Button>
+          <br></br>
+          <br></br>
+          <Button
+            className="cakesButton"
+            href="#cakes"
+          > Cakes
+          </Button>
+          </div>
+        <div id="cakes">
+          <CakeList
+            cakes={this.props.cakes}
+            showCake={this.props.showCake}
+            findCake={this.props.findCake}
+          />
+          <br></br>
+        </div>
       </div>
     );
   }
