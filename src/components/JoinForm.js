0af9ee6653ea91fd2c9f7b535995343b0cb43c8a
claudiasolbes <cssolbes@hotmail.com>
2019-12-15 19:49:13 -0500

    fixed join page's height

--- src/components/JoinForm.js
+++ src/components/JoinForm.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { Button, Form, Grid, Segment } from "semantic-ui-react";
+import { Button, Form, Segment } from "semantic-ui-react";
 import swal from 'sweetalert';
 
 class JoinForm extends Component{
